fe6b808473c7b186b975bd96965a75d9883dda3a
Sheela N <113254587+Sheelajsm@users.noreply.github.com>
2022-09-27 22:04:54 +0530

    Add files via upload

---
+++ Assignment6.py
@@ -0,0 +1,58 @@
+# 1. what is a decorator in Python, explain with example. 
+  # Decorator allows programmers to modify the behaviour of a function or class.
+  
+# 2.	What is closure in python
+    # Python Closures are inner functions that are enclosed within the outer function. 
+      
+# 3. 	When to use Python Closures and their benefits
+    # Closures can access variables present in the outer function. 
+    # It can access these variables even after the outer function has completed its execution
+    
+# 4. 	What is module in python
+    # Module is a library in Python consisting of classes, functions and variables.
+    
+# 5.	What is an iterator in python
+     # an iterator is an object which implements the iterator protocol, which 
+     # consist of the methods __iter__() and __next__() .
+     
+# 6.	print the current date and time using 'datetime' module along with month name
+# import datetime as dt
+
+# current_date = dt.date.today()
+# print("Today's date:", current_date)
+
+# current_datetime = dt.datetime.now()
+# print(current_datetime)
+
+# o/p:
+# Today's date: 2022-09-26
+# 2022-09-26 18:29:20.325602
+
+
+# 7. loop through a nested folder and print all the file name
+
+
+
+
+
+# 8. 	Create a directory, write a text file and append it.
+        # We create a directory by using a syntax in cmd:
+       # mkdir <folder name>
+import requests
+      
+file_ = open('txt file',w)
+
+print(file_.write('This is my Diary'))
+       
+# 9.	What is PIP
+   # PIP  is a package manager for Python. We can install or remove or unistall any packages, modules or library.
+   
+ # 10)How to install and uninstall a package/module in python
+  # We can install a package by using the syntax in cmd:
+  # PIP unistall <packagename>
+  # PIP install <packagename>
+  # PIP remove <packagename>
+  
+# 11. Using 'requests' module fetch the contents of https://itdefine.in/ in web.txt file
+      
+# 12. Search the phone number and email address in web.txt file
